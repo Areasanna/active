d7adc4c24a8fdc0fef964d214e7a54f296b762df
ana cunha <ana.cunha@zup.com.br>
2026-05-05 10:23:02 -0300

    feat(auth): implementar AuthService para geração de token jwt usando loginrequest e loginresponse

---
+++ active/src/main/java/com/example/active/auth/AuthService.java
@@ -0,0 +1,52 @@
+package com.example.active.auth;
+
+import com.example.active.usuario.LoginRequest;
+import com.example.active.usuario.LoginResponse;
+import com.example.active.usuario.Usuario;
+import com.example.active.usuario.UsuarioRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UsuarioRepository usuarioRepository;
+    private final AuthenticationManager authenticationManager;
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    private final long jwtExpirationMs = 86400000; // 1 dia
+
+    public LoginResponse loguin(LoginRequest request) {
+        Usuario usuario = usuarioRepository.findByEmail(request.email())
+                .orElseThrow(() -> new BadCredentialsException("Credenciais inválidas"));
+        if (!passwordEncoder().matches(request.password(), usuario.getPassword())) {
+            throw new BadCredentialsException("Credenciais inválidas");
+        }
+
+        String token = Jwts.builder()
+                .setSubject(usuario.getEmail())
+                .claim("usuarioId", usuario.getId())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+        return new LoginResponse(token, "Bearer");
+
+    }
+
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
