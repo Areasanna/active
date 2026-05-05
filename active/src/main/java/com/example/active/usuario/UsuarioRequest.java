0a5bc409de63e1ec3daa80631f5fb0a2d57197dc
ana cunha <ana.cunha@zup.com.br>
2026-05-05 08:04:56 -0300

    fix(usuarioRequest): corrigir mensagens de validação de obrigatoriedade e ortografia

--- active/src/main/java/com/example/active/usuario/UsuarioRequest.java
+++ active/src/main/java/com/example/active/usuario/UsuarioRequest.java
@@ -5,15 +5,15 @@ import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
 
 public class UsuarioRequest{
-    @NotBlank(message = "O nome é obrigadorio")
+    @NotBlank(message = "O nome é obrigatório")
     @Size(min = 2, max = 100)
     private String nome;
 
-    @NotBlank(message = "E-mail é obrigadorio")
+    @NotBlank(message = "E-mail é obrigatório")
     @Email(message = "Informe um e-mail válido")
     private String email;
 
-    @NotBlank(message = " A senha é obrigadoria")
+    @NotBlank(message = " A senha é obrigatória")
     @Size(min = 8, message = "Senha tem que ter no mínimo 8 caracteres")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).+$",
             message = "A senha deve ter letras e números")
