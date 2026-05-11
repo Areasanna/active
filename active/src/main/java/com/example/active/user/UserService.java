1b9b4aa970092f7d9d929acce4f1de9d2e069be3
ana cunha <ana.cunha@zup.com.br>
2026-05-11 20:08:01 -0300

    feat(user): implementar serviço com operações de listagem, criação, leitura, atualização e deleção de usuários usando criptografia de senha

---
+++ active/src/main/java/com/example/active/user/UserService.java
@@ -0,0 +1,100 @@
+package com.example.active.user;
+
+import com.example.active.exception.EmailCadastrado;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userrepository;
+    //nunca armazenar senhas em texto puro
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+
+    @Transactional
+    public Page<UserResponse> list(Pageable pageable) {
+        return userrepository.findAll(pageable)
+                .map(this::toResponse);
+    }
+
+    @Transactional
+    public UserResponse cadastrar(UserRequest request) {
+        if (userrepository.existsByEmail(request.getEmail())) {
+            throw new EmailCadastrado("E-mail já cadastrado");
+        }
+        if (!request.getPassword().matches("^(?=.*[A-Za-z])(?=.*\\d).{8,}$")) {
+            throw new RuntimeException("Senha deve conter letras e números e no mínimo 8 caracters");
+        }
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setHeight(request.getHeight());
+        user.setAge(request.getAge());
+        user.setWeight(request.getWeight());
+        user.setTrainingLevel(request.getTrainingLevel());
+
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user = userrepository.save(user);
+        userrepository.flush();
+
+        return toResponse(user);
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponse buscarporId(Long id) {
+        User user = userrepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado com id: " + id));
+        return toResponse(user);
+    }
+
+    @Transactional
+    public void deletarPorId(Long id) {
+        User user = userrepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado com id:" + id));
+        userrepository.delete(user);
+    }
+
+    @Transactional
+    public UserResponse atualizarPorId(Long id, UserRequest request) {
+        User user = userrepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado com id: " + id));
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setHeight(request.getHeight());
+        user.setAge(request.getAge());
+        user.setWeight(request.getWeight());
+        user.setTrainingLevel(request.getTrainingLevel());
+
+        if (request.getPassword() != null && !request.getPassword().isBlank()) {
+            if (!request.getPassword().matches("^(?=.*[A-Za-z])(?=.*\\d).{8,}$")) {
+                throw new RuntimeException("Senha deve conter letras e números e no mínimo 8 caracteres");
+            }
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+
+        user = userrepository.save(user);
+
+        return toResponse(user);
+    }
+
+    private UserResponse toResponse(User user) {
+        return new UserResponse(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getTrainingLevel(),
+                user.getAge(),
+                user.getWeight(),
+                user.getHeight(),
+                user.getCreatedAt()
+        );
+    }
+}
+
