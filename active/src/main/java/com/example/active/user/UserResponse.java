ccb90075c09f3ec5fa16df72b51a4543f3f5d36a
ana cunha <ana.cunha@zup.com.br>
2026-05-11 20:07:35 -0300

    refactor(user): padronizar UserResponse e adicionar campos age, weight e height

--- active/src/main/java/com/example/active/user/UserResponse.java
+++ active/src/main/java/com/example/active/user/UserResponse.java
@@ -1,17 +1,18 @@
-package com.example.active.usuario;
+package com.example.active.user;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-
-public record UsuarioResponse (
+public record UserResponse(
         Long id,
-        String nome,
+        String name,
         String email,
         TrainingLevel trainingLevel,
+        Integer age,
+        BigDecimal weight,
+        BigDecimal height,
         //formatação para a data sair mais legível
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime createdAt
-
 ){}
