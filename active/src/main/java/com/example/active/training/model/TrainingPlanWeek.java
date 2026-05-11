c857ab3b61ad34672a37a4b4d3913924a06b7b58
ana cunha <ana.cunha@zup.com.br>
2026-05-11 20:03:47 -0300

    feat(training): adicionar entidade trainingplanweek com restrição unique por semana e plano

---
+++ active/src/main/java/com/example/active/training/model/TrainingPlanWeek.java
@@ -0,0 +1,31 @@
+package com.example.active.training.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "training_plan_weeks",  uniqueConstraints = @UniqueConstraint(columnNames = {"training_plan_id", "week_number"}))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TrainingPlanWeek {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "week_number", nullable = false)
+    private Integer weekNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "training_plan_id", nullable = false)
+    private TrainingPlan trainingPlan;
+
+    @OneToMany(mappedBy = "trainingPlanWeek", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<TrainingPlanDay> days = new ArrayList<>();
+}
