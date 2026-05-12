d04d6520868db5c52e8829964381ec9594239085
ana cunha <ana.cunha@zup.com.br>
2026-05-12 18:50:31 -0300

    feat(equipment.repository): adicionar JpaSpecificationExecutor para suportar specs

--- active/src/main/java/com/example/active/equipment/repository/EquipmentRepository.java
+++ active/src/main/java/com/example/active/equipment/repository/EquipmentRepository.java
@@ -2,6 +2,7 @@ package com.example.active.equipment.repository;
 
 import com.example.active.equipment.model.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+public interface EquipmentRepository extends JpaRepository<Equipment, Long>, JpaSpecificationExecutor<Equipment> {
 }
