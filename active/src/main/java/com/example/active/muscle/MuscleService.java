6b2ed28891b07bbcbc520a53e5fe75463a11441f
ana cunha <ana.cunha@zup.com.br>
2026-05-12 11:18:33 -0300

    feat(muscle.service): adicionar validação de nome no método create do MuscleService

--- active/src/main/java/com/example/active/muscle/MuscleService.java
+++ active/src/main/java/com/example/active/muscle/MuscleService.java
@@ -7,7 +7,9 @@ import com.example.active.muscle.repository.MuscleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -20,10 +22,17 @@ public class MuscleService {
     }
 
     public MuscleResponse create(MuscleRequest request) {
+        validateMuscleRequest(request);
         var muscle = Muscle.builder()
                 .name(request.name())
                 .build();
         repository.save(muscle);
         return new MuscleResponse(muscle.getId(), muscle.getName(), muscle.getNameEn());
+    }
+
+    private void validateMuscleRequest(MuscleRequest request) {
+        if (request.name() == null || request.name().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O nome do músculo é obrigatório");
+        }
     }
 }
