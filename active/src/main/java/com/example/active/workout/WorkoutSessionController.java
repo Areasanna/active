fe3aed1231bba6e6853700ddaa2ea6d4001cf91c
ana cunha <ana.cunha@zup.com.br>
2026-05-12 19:01:16 -0300

    feat(workout-session.controller): adicionar filtros por data e nome do treino com paginação e logging

--- active/src/main/java/com/example/active/workout/WorkoutSessionController.java
+++ active/src/main/java/com/example/active/workout/WorkoutSessionController.java
@@ -1,12 +1,23 @@
 package com.example.active.workout;
 
+import com.example.active.training.TrainingPlanController;
 import com.example.active.user.model.User;
 import com.example.active.workout.dto.WorkoutSessionCreateRequest;
 import com.example.active.workout.dto.WorkoutSessionCreateResponse;
+import com.example.active.workout.model.WorkoutSession;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import net.kaczmarzyk.spring.data.jpa.domain.Between;
+import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +29,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class WorkoutSessionController {
     private final WorkoutSessionService service;
+    private static final Logger logger = LoggerFactory.getLogger(WorkoutSessionController.class);
 
     @PostMapping
     public ResponseEntity<WorkoutSessionCreateResponse.WorkoutSessionResponse> register(
@@ -30,9 +42,16 @@ public class WorkoutSessionController {
     @GetMapping
     public ResponseEntity<Page<WorkoutSessionCreateResponse.WorkoutSessionResponse>> list(
             @AuthenticationPrincipal User userAutenticado,
-            @PageableDefault(size = 10, sort = "date") Pageable pageable) {
+            @Parameter(hidden = true)
+            @And({
+                    @Spec(path = "date", params = {"fromDate", "toDate"}, spec = Between.class),
+                    @Spec(path = "workoutName", spec = LikeIgnoreCase.class)
+            }) Specification<WorkoutSession> spec,
+            @PageableDefault(size = 10, sort = "date", direction = Sort.Direction.DESC) Pageable pageable) {
 
-        return ResponseEntity.ok(service.list(userAutenticado, pageable));
+        logger.info("Get/sessoes-treino - Usuário: {} - Paginação: {}", userAutenticado.getId(), pageable);
+
+        return ResponseEntity.ok(service.list(userAutenticado, spec, pageable));
     }
 
     @GetMapping("/{id}")
