6a25fafbf590166038f61ab5fda4c71ec67dacfc
ana cunha <ana.cunha@zup.com.br>
2026-05-13 10:01:52 -0300

    feat(equipment): adicionar método existsByName ao repositório de equipamentos

--- active/src/main/java/com/example/active/equipment/repository/EquipmentRepository.java
+++ active/src/main/java/com/example/active/equipment/repository/EquipmentRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface EquipmentRepository extends JpaRepository<Equipment, Long>, JpaSpecificationExecutor<Equipment> {
+    boolean existsByName(String name);
 }
