6757b5040cc78ce9d9e630d68ff2a45e7c8ebf6d
ana cunha <ana.cunha@zup.com.br>
2026-05-18 10:44:53 -0300

    test(activeapplication): ativar perfil de teste para testes de contexto

--- active/src/test/java/com/example/active/ActiveApplicationTests.java
+++ active/src/test/java/com/example/active/ActiveApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.active;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Isso faz o Spring buscar especificamente o application-test.properties
 class ActiveApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
